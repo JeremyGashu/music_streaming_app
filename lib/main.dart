528fdd276b920ddab0cfea7b60c8d3e13f56f07a
JeremyGashu <gearmias@gmail.com>
2021-04-01 23:35:52 +0300

    feature push_notification fixes #2

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+  await initMessaging();
 
   FlutterError.onError = (FlutterErrorDetails details) {
     FirebaseCrashlytics.instance.log(details.toString());
@@ -20,30 +24,43 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    Firebase.initializeApp().whenComplete(() {
-      print("completed");
-      setState(() {});
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: Firebase.initializeApp(),
-        builder: (_, snapshot) {
-          return MaterialApp(
-            title: 'Material App',
-            home: Scaffold(
-              appBar: AppBar(
-                title: Text('Material App Bar'),
-              ),
-              body: Center(
-                child: Container(
-                  child: Text('Hello World'),
-                ),
-              ),
-            ),
-          );
-        });
+    return MaterialApp(
+        title: 'Material App',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Material App Bar'),
+          ),
+          body: Center(
+            child: Text('Hello World'),
+          ),
+        ));
   }
+}
+
+initMessaging() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  String token = await messaging.getToken();
+  print("USER TOKEN:" + token);
+
+  FirebaseMessaging.onMessage.listen((message) {
+    print('NOTIFICATION RECEIVED');
+    print('TITLE: ' + message.notification.title);
+    print('BODY: ' + message.notification.body);
+  });
+
+  print('User granted permission: ${settings.authorizationStatus}');
 }
